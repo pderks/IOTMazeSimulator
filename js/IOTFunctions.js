b5b03f100c10f85fc2ccf21a66cbb2ec62d3423c
harshilasu <harshilpatel@harshils-mbp.mobile.asu.edu>
2016-01-27 16:30:06 -0700

    MOVEMENT OF ROBOT FINALIZED

--- js/IOTFunctions.js
+++ js/IOTFunctions.js
@@ -4,6 +4,7 @@ function popup(){
     alert("Hello World");
 }
 function startServer(){
+    var direction = 0;
     alert("Starting Server");
     //connect to viple
     connection = new WebSocket("ws://localhost:8124");
@@ -19,12 +20,76 @@ function startServer(){
      //alert("LISTENING SERVER");
     connection.onmessage = function(evt){
         var received = evt.data;
-        up();
+        var stringfy = received.toString();
+        //alert(stringfy);
+       // stringfy = "up down left right";
+        //if(stringfy.indexOf("{\"servos\":[{\"isTurn\":false,\"servoID\":3,\"servoSpeed\":0},{\"isTurn\":false,\"servoId\":5,\"servoSpeed\":0}]}") > -1){
+       var first = stringfy.split(":");
+       
+       
+       
+       
+       ///////////lEFT/DOWN//////////////////
+        if(first[4].indexOf("-0.5") > -1){
+            if(first[7].indexOf("-0.5")){
+                if(direction == 3){
+                    direction = 0;
+                }
+                 else{
+                     direction++;
+                 }
+
+              
+            }
+            else{
+                alert(direction);
+                if(direction == 0){
+                    moveDown();
+                }
+                if(direction == 1){
+                    moveRight();
+                }
+                if(direction == 2){
+                    moveUp();
+                }
+                if(direction == 3){
+                    moveLeft();
+            }          
+        }
+    }
+       ///////RIGHT/UP//////////////////////
+      else if(first[4].indexOf("0.5") > -1){
+            if(first[7].indexOf("-0.5")){
+                if(direction == 0){
+                    moveUp();
+                }
+                if(direction == 1){
+                    moveLeft();
+                }
+                if(direction == 2){
+                    moveDown();
+                }
+                if(direction == 3){
+                    moveRight();
+                }  
+            }
+            else{
+                if(direction == 0){
+                    direction = 3;
+                }
+                else{
+                    direction--;
+                    
+                }
+                
+            }
+        }
         
-        move();
+         
+     
     }
     
-    checker = checker+1;
+    
 }
 
 
