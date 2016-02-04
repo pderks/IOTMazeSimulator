9e82005c115ef2992994ac24cdb592eeff65352a
pderks <p.derks@me.com>
2016-02-03 21:19:05 -0700

    Deleted junk at the bottom of the page. Took out buttons that weren't needed, but left functions for those buttons incase needed later. Resized maze to fit within the constraints of the maze header. Deleted green square from the maze. Organized, formated and commented code.

--- js/IOTFunctions.js
+++ js/IOTFunctions.js
@@ -1,112 +1,96 @@
 
 var connection;
-function popup(){
-    alert("Hello World");
-}
+
 function startServer(){
     var direction = 0;
     alert("Starting Server");
-    //connect to viple
+    //connect to VIPLE
     connection = new WebSocket("ws://localhost:8124");
-    connection.onopen = function(){
     
+    connection.onopen = function(){
         document.getElementById("testing").innerHTML = 40;
     }
+    
     if ("WebSocket" in window)
     {
         //alert("WebSocket is supported by your Browser!");
     }
     
-     //alert("LISTENING SERVER");
     connection.onmessage = function(evt){
         var received = evt.data;
         var stringfy = received.toString();
         //alert(stringfy);
-       // stringfy = "up down left right";
+        // stringfy = "up down left right";
         //if(stringfy.indexOf("{\"servos\":[{\"isTurn\":false,\"servoID\":3,\"servoSpeed\":0},{\"isTurn\":false,\"servoId\":5,\"servoSpeed\":0}]}") > -1){
-       var first = stringfy.split(":");
-       
-       
-       
-       
-       ///////////lEFT/DOWN//////////////////
+        var first = stringfy.split(":");
+
+        //Cases for Rotate Left and Move Down
         if(first[4].indexOf("-0.5") > -1){
             if(first[7].indexOf("-0.5")){
-                if(direction == 3){
-                    
+                //Case to Rotate Left
+                if(direction == 3){ 
                     makeWhite(currRectX, currRectY, rWidth,rHeight);
                     swapDim();
                     drawRectangle(newX, newY, "#0000FF");
                     direction = 0;
                 }
-                 else{
-                      makeWhite(currRectX, currRectY, rWidth,rHeight);
+                else{
+                    makeWhite(currRectX, currRectY, rWidth,rHeight);
                     swapDim();
                     drawRectangle(newX, newY, "#0000FF");
-                     direction++;
-                 }
-
-              
+                    direction++;
+                 }  
             }
+            //Case to Move Down
             else{
-                alert(direction);
                 if(direction == 0){
                     moveDown();
                 }
-                if(direction == 1){
-                    
+                if(direction == 1){   
                     moveRight();
                 }
                 if(direction == 2){
                     moveUp();
                 }
                 if(direction == 3){
-                   
                     moveLeft();
-            }          
+                }          
+            }
         }
-    }
-       ///////RIGHT/UP//////////////////////
-      else if(first[4].indexOf("0.5") > -1){
+        //Cases for Rotate Right and Move Up
+        else if(first[4].indexOf("0.5") > -1){
+            //Case to Move Up
             if(first[7].indexOf("-0.5")){
                 if(direction == 0){
                     moveUp();
                 }
-                if(direction == 1){
-                    
+                if(direction == 1){                    
                     moveLeft();
                 }
                 if(direction == 2){
                     moveDown();
                 }
-                if(direction == 3){
-                     
+                if(direction == 3){    
                     moveRight();
                 }  
             }
+            //Case to Rotate Right
             else{
                 if(direction == 0){
-                     makeWhite(currRectX, currRectY, rWidth,rHeight);
+                    makeWhite(currRectX, currRectY, rWidth,rHeight);
                     swapDim();
                     drawRectangle(newX, newY, "#0000FF");
                     direction = 3;
                 }
                 else{
-                     makeWhite(currRectX, currRectY, rWidth,rHeight);
+                    makeWhite(currRectX, currRectY, rWidth,rHeight);
                     swapDim();
                     drawRectangle(newX, newY, "#0000FF");
-                    direction--;
-                    
-                }
-                
+                    direction--;  
+                }   
             }
-        }
-        
-         
-     
-    }
-    
-    
+        }     
+    }    
 }
 
 
@@ -123,7 +107,7 @@ function up(){
 }
 
 function down(){
-        document.getElementById("left").innerHTML = -0.5;
+    document.getElementById("left").innerHTML = -0.5;
     document.getElementById("right").innerHTML = -0.5;
 
     connection.send('down');
@@ -136,13 +120,13 @@ function left(){
 }
 
 function right(){
-   document.getElementById("left").innerHTML = 0.5;
+    document.getElementById("left").innerHTML = 0.5;
     document.getElementById("right").innerHTML = -0.5;
     connection.send('right');
 }
 
 function stop(){
-     document.getElementById("left").innerHTML = 0.0;
+    document.getElementById("left").innerHTML = 0.0;
     document.getElementById("right").innerHTML = 0.0;
     connection.send('stop');
 }    
